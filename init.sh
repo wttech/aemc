df8a5c2aef2450adff3443ea33ace7c70e635944
Krystian Panek <krystian.panek@wundermanthompson.com>
2022-12-14 21:52:42 +0100

    Init script fixed

--- init.sh
+++ init.sh
@@ -6,20 +6,24 @@ SCRIPT_DIR=${AEM_DIR}/script
 WRAPPER_SCRIPT=aemw
 CONFIG_FILE=aem/home/config.yml
 
-if [ ! -f "$WRAPPER_SCRIPT" ]; then
-  echo "Downloading AEM Compose Files"
-  echo ""
-
-  mkdir -p "$SCRIPT_DIR" && \
-  curl -s ${SOURCE_URL}/${SCRIPT_DIR}/destroy.sh -o ${SCRIPT_DIR}/destroy.sh && \
-  curl -s ${SOURCE_URL}/${SCRIPT_DIR}/down.sh -o ${SCRIPT_DIR}/down.sh && \
-  curl -s ${SOURCE_URL}/${SCRIPT_DIR}/resetup.sh -o ${SCRIPT_DIR}/resetup.sh && \
-  curl -s ${SOURCE_URL}/${SCRIPT_DIR}/restart.sh -o ${SCRIPT_DIR}/restart.sh && \
-  curl -s ${SOURCE_URL}/${SCRIPT_DIR}/setup.sh -o ${SCRIPT_DIR}/setup.sh && \
-  curl -s ${SOURCE_URL}/${SCRIPT_DIR}/up.sh -o ${SCRIPT_DIR}/up.sh && \
-  curl -s ${SOURCE_URL}/${AEM_DIR}/api.sh -o ${AEM_DIR}/api.sh && \
-  curl -s ${SOURCE_URL}/${WRAPPER_SCRIPT} -o ${WRAPPER_SCRIPT}
+if [ -f "$WRAPPER_SCRIPT" ]; then
+  echo "The project contains already AEM Compose!"
+  exit 1
 fi
+
+echo "Downloading AEM Compose Files"
+echo ""
+
+mkdir -p "$SCRIPT_DIR"
+curl -s ${SOURCE_URL}/${SCRIPT_DIR}/destroy.sh -o ${SCRIPT_DIR}/destroy.sh
+curl -s ${SOURCE_URL}/${SCRIPT_DIR}/down.sh -o ${SCRIPT_DIR}/down.sh
+curl -s ${SOURCE_URL}/${SCRIPT_DIR}/resetup.sh -o ${SCRIPT_DIR}/resetup.sh
+curl -s ${SOURCE_URL}/${SCRIPT_DIR}/restart.sh -o ${SCRIPT_DIR}/restart.sh
+curl -s ${SOURCE_URL}/${SCRIPT_DIR}/setup.sh -o ${SCRIPT_DIR}/setup.sh
+curl -s ${SOURCE_URL}/${SCRIPT_DIR}/up.sh -o ${SCRIPT_DIR}/up.sh
+curl -s ${SOURCE_URL}/${AEM_DIR}/api.sh -o ${AEM_DIR}/api.sh
+curl -s ${SOURCE_URL}/${WRAPPER_SCRIPT} -o ${WRAPPER_SCRIPT}
+
 
 echo "Downloading AEM Compose CLI"
 echo ""

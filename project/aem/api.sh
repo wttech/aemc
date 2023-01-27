1dceb2d1f671bf7f1960b59a40080be5059614ff
Krystian Panek <krystian.panek@wundermanthompson.com>
2023-01-27 15:23:18 +0100

    Release v0.12.4

--- project/aem/api.sh
+++ project/aem/api.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-VERSION=${AEMC_VERSION:-"0.12.3"}
+VERSION=${AEMC_VERSION:-"0.12.4"}
 
 # Utilities
 
--- project/init.sh
+++ project/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-VERSION=${AEMC_VERSION:-"0.12.3"}
+VERSION=${AEMC_VERSION:-"0.12.4"}
 SOURCE_URL="https://raw.githubusercontent.com/wttech/aemc/v${VERSION}/project"
 
 AEM_WRAPPER="aemw"

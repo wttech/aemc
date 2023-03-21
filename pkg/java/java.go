2fbe8fe40e14aa1d952995dfad554bdf22518d1b
Krystian Panek <krystian.panek@wundermanthompson.com>
2023-03-21 10:30:26 +0100

    Bugfix

--- pkg/java/java.go
+++ pkg/java/java.go
@@ -164,7 +164,7 @@ func (o *Opts) Env() ([]string, error) {
 		return nil, err
 	}
 	javaPath := pathx.Canonical(homeDir + "/bin")
-	envOthers := osx.EnvVarsWithout("PATH", "JAVA_HOME", "TERM", "PROMPT")
+	envOthers := osx.EnvVarsWithout("PATH", "JAVA_HOME")
 	envFinal := append([]string{
 		"PATH=" + javaPath + ":" + os.Getenv("PATH"),
 		"JAVA_HOME=" + homeDir,

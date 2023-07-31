3231de37416486459a282bc65b24e3c79c73c381
Piotr Andruszkiewicz <piotr.andruszkiewicz@wundermanthompson.com>
2023-07-31 08:13:27 +0200

    Add support for SSL by Default: Mark CLI flags required

--- cmd/aem/ssl.go
+++ cmd/aem/ssl.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/wttech/aemc/pkg"
 	"github.com/wttech/aemc/pkg/common/mapsx"
@@ -35,12 +34,6 @@ func (c *CLI) sslSetupCmd() *cobra.Command {
 			httpsHostname, _ := cmd.Flags().GetString("https-hostname")
 			httpsPort, _ := cmd.Flags().GetString("https-port")
 
-			err = validateSSLSetupFlags(keyStorePassword, trustStorePassword, certificateFile, privateKeyFile, httpsHostname, httpsPort)
-			if err != nil {
-				c.Error(err)
-				return
-			}
-
 			configured, err := pkg.InstanceProcess(c.aem, instances, func(instance pkg.Instance) (map[string]any, error) {
 				changed, err := instance.SSL().Setup(keyStorePassword, trustStorePassword, certificateFile, privateKeyFile, httpsHostname, httpsPort)
 				if err != nil {
@@ -70,27 +63,11 @@ func (c *CLI) sslSetupCmd() *cobra.Command {
 	cmd.Flags().String("private-key-file", "", "Private key file (DER format)")
 	cmd.Flags().String("https-hostname", "", "HTTPS hostname")
 	cmd.Flags().String("https-port", "", "HTTPS port")
+	_ = cmd.MarkFlagRequired("keystore-password")
+	_ = cmd.MarkFlagRequired("truststore-password")
+	_ = cmd.MarkFlagRequired("certificate-file")
+	_ = cmd.MarkFlagRequired("private-key-file")
+	_ = cmd.MarkFlagRequired("https-hostname")
+	_ = cmd.MarkFlagRequired("https-port")
 	return cmd
-}
-
-func validateSSLSetupFlags(keyStorePassword, trustStorePassword, certificateFile, privateKeyFile, httpsHostname, httpsPort string) error {
-	if len(keyStorePassword) == 0 {
-		return fmt.Errorf("Keystore password is required")
-	}
-	if len(trustStorePassword) == 0 {
-		return fmt.Errorf("Truststore password is required")
-	}
-	if len(certificateFile) == 0 {
-		return fmt.Errorf("Certificate file is required")
-	}
-	if len(privateKeyFile) == 0 {
-		return fmt.Errorf("Private key file is required")
-	}
-	if len(httpsHostname) == 0 {
-		return fmt.Errorf("HTTPS hostname is required")
-	}
-	if len(httpsPort) == 0 {
-		return fmt.Errorf("HTTPS port is required")
-	}
-	return nil
 }

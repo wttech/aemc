c52ad7a74efed37073166cb338603101bf0e2444
Piotr Andruszkiewicz <piotr.andruszkiewicz@wundermanthompson.com>
2023-07-31 08:27:17 +0200

    Add support for SSL by Default: Retrieve error message from response

--- pkg/ssl.go
+++ pkg/ssl.go
@@ -7,6 +7,9 @@ import (
 	"github.com/wttech/aemc/pkg/common/filex"
 	"github.com/wttech/aemc/pkg/common/osx"
 	"github.com/wttech/aemc/pkg/common/pathx"
+	"golang.org/x/net/html"
+	"io"
+	"strings"
 )
 
 const (
@@ -70,7 +73,17 @@ func (s SSL) Setup(keyStorePassword, trustStorePassword, certificateFile, privat
 	if err != nil {
 		return false, fmt.Errorf("%s > failed to setup SSL: %w", s.instance.ID(), err)
 	} else if response.IsError() {
-		return false, fmt.Errorf("%s > failed to setup SSL: %s", s.instance.ID(), response.Status())
+		// If the response is an error, we try to read the error message from the response body.
+		// The resty library does not provide a way to read the response body if the response is an error.
+		body, err := io.ReadAll(response.RawBody())
+		if err != nil {
+			return false, fmt.Errorf("%s > failed to setup SSL: %s, %w", s.instance.ID(), response.Status(), err)
+		}
+		errorMessage, err := findErrorMessage(string(body[:]))
+		if err != nil {
+			return false, fmt.Errorf("%s > failed to setup SSL: %s, %w", s.instance.ID(), response.Status(), err)
+		}
+		return false, fmt.Errorf("%s > failed to setup SSL: %s, %s", s.instance.ID(), response.Status(), errorMessage)
 	}
 
 	if err := lock.Lock(); err != nil {
@@ -78,6 +91,63 @@ func (s SSL) Setup(keyStorePassword, trustStorePassword, certificateFile, privat
 	}
 
 	return true, nil
+}
+
+// From HTML response body, e.g.:
+// <!DOCTYPE html>
+// <html lang='en'>
+// <head>
+// <title>Error</title>
+// </head>
+// <body>
+// <h1>Error</h1>
+// <dl>
+// <dt class='foundation-form-response-status-code'>Status</dt>
+// <dd>500</dd>
+// <dt class='foundation-form-response-status-message'>Message</dt>
+// <dd>Invalid password for existing key store</dd>
+// <dt class='foundation-form-response-title'>Title</dt>
+// <dd>Error</dd>
+// </dl>
+// </body>
+// </html>
+// returns "Invalid password for existing key store"
+func findErrorMessage(body string) (errorMessage string, err error) {
+	doc, err := html.Parse(strings.NewReader(body))
+	if err != nil {
+		return "", err
+	}
+
+	var traverse func(*html.Node)
+	traverse = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "dt" && hasClass(n, "foundation-form-response-status-message") {
+			// If the <dt> node with class 'foundation-form-response-status-message' is found,
+			// we search for the corresponding <dd> node containing the error message.
+			ddNode := n.NextSibling
+			for ddNode != nil {
+				if ddNode.Type == html.ElementNode && ddNode.Data == "dd" {
+					errorMessage = ddNode.FirstChild.Data
+					return
+				}
+				ddNode = ddNode.NextSibling
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
+	}
+
+	traverse(doc)
+	return errorMessage, nil
+}
+
+func hasClass(n *html.Node, class string) bool {
+	for _, attr := range n.Attr {
+		if attr.Key == "class" {
+			return strings.Contains(attr.Val, class)
+		}
+	}
+	return false
 }
 
 func (s SSL) lock(keyStorePassword, trustStorePassword, certificateFile, privateKeyFile, httpsHostname, httpsPort string) osx.Lock[sslLock] {

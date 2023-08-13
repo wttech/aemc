77371cb3a23e0ddbbacf7320cffad660fa64a69a
Krystian Panek <krystian.panek@wundermanthompson.com>
2023-08-13 22:50:54 +0200

    Project init using taskfile, no APM by default

--- project-init.sh
+++ project-init.sh
@@ -14,14 +14,21 @@ curl -s "${SOURCE_URL}/${AEM_WRAPPER}" -o "${AEM_WRAPPER}"
 curl -s "${SOURCE_URL}/${TASK_WRAPPER}" -o "${TASK_WRAPPER}"
 
 echo ""
-echo "Downloading & Testing AEM Compose CLI"
+echo "Downloading & Testing AEM Compose CLI (https://github.com/wttech/aemc)"
 echo ""
 
-chmod +x "${AEM_WRAPPER}" "${TASK_WRAPPER}"
+chmod +x "${AEM_WRAPPER}"
 sh ${AEM_WRAPPER} version
+
+echo ""
+echo "Downloading & Testing Task Tool (https://github.com/go-task/task)"
+echo ""
+
+chmod +x "${TASK_WRAPPER}"
+sh ${TASK_WRAPPER} --version
 
 echo ""
 echo "Success! Now initialize the project by running command below:"
 echo ""
 
-echo "sh ${AEM_WRAPPER} init"
+echo "sh ${TASK_WRAPPER} init"

12710ab8e10e6199316b1395dbe023a2a78dba35
Krystian Panek <krystian.panek@vml.com>
2025-07-30 08:01:31 +0200

    Package install extract only when snapshot

--- pkg/pkg/constants.go
+++ pkg/pkg/constants.go
@@ -3,4 +3,6 @@ package pkg
 const (
 	InstallSuccess           = "<span class=\"Package imported.\">"
 	InstallSuccessWithErrors = "<span class=\"Package imported (with errors"
+
+	InstallExtractOnlySnapshot = "snapshot"
 )
